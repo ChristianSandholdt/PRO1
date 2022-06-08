39ed7540e965692d98bf7ba6196b77c6f8cbad68
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2022-06-08 16:53:54 +0200

    Prøveeksamen

--- L-36-Semesterproeve2018/src/gui/GUI.java
+++ L-36-Semesterproeve2018/src/gui/GUI.java
@@ -16,6 +16,7 @@ public class GUI extends Application {
 
     public static void main(String[] args) {
         Application.launch(args);
+
     }
     @Override
     public void start(Stage stage) {
